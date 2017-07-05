e4f91c368cf41dda62e0670f87cbb14d1fe40da6
Admin <kosyak_@mail.ru>
2017-07-05 15:34:09 +0300

    Initial Commit

---
+++ HW7_NataliaSinitsyna/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW7_NataliaSinitsyna
+//
+//  Created by Admin on 05.07.17.
+//  Copyright © 2017 GB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
